575b12766679c76d8231f4f48b7a48e0cc75ea1f
KAUA PEREIRA <54439995837@info.local>
2024-06-10 17:27:40 -0300

    mapa do jogo

--- src/resources.ts
+++ src/resources.ts
@@ -1,17 +1,40 @@
 import { ImageSource, Loader } from "excalibur";
 import sword from "./images/sword.png";
+import { TiledResource } from "@excaliburjs/plugin-tiled";
 import logo from "./images/logo.png";
 import logoVertical from "./images/logo-vertical.png";
 import controle from "./images/controle.png"
+
+import pngTilesetPath from "./maps/Room_Builder_32x32.png?url"
+
+import tsxParedesPath from "./maps/tileset_Room?url"
+import tsxGenericPath from "./maps/tileset_generic?url"
+import tsxEstoquePath from "./maps/tileset_basement?url"
+import tsxBiblioteca from "./maps/tileset_biblioteca?url"
+
+import tmxMapaPath from "./maps/mapa.tmx?url"
+
 
 export const Resources = {
   Sword: new ImageSource(sword),
   Logo: new ImageSource(logo),
   LogoVertical: new ImageSource (logoVertical),
-  Controle: new ImageSource (controle)
+  Controle: new ImageSource (controle),
+  Mapa: new TiledResource(tmxMapaPath, {
+    pathMap: [
+      {path: "mapa.tmx", output: tmxMapaPath},
+      {path: "Room_Builder_32x32.png", output: pngTilesetPath},
+      {path: "tileset_Room", output: tsxParedesPath},
+      {path: "tileset_generic", output: tsxGenericPath},
+      {path: "tileset_basement", output: tsxEstoquePath},
+      {path: "tileset_biblioteca", output: tsxBiblioteca}
+
+    ]
+  })
 } as const;
 
 export const loader = new Loader();
 for (const res of Object.values(Resources)) {
   loader.addResource(res);
+
 }
